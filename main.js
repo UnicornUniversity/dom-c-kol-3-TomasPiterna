7b72f01b441c2a80a8fbecb85562841e803ca357
Tomáš Piterna <piterna.tomas@seznam.cz>
2026-04-12 16:57:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -22,9 +22,33 @@ const femaleSurnames = [
 ];
 const workload = [10, 20, 30, 40];
   
+//returns a random whole number between min and max  
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+//returns random item for an array
+function randomFrom(arr) {
+  return arr[randomInt(0, arr.length - 1)];
+}
+
+//returns a random birthdate based on the age range
+function randomBirthdate(minAge, maxAge) {
+  const today = new Date();
+//one year in miliseconds
+const oneYearMs = 365.25 * 24 * 60 * 60 * 1000
+
+//the oldest possible birthdate calculated by substracting maxAge years from today's date
+const oldestBirthdate = new Date (today - maxAge * oneYearMs);
+
+//the youngest possible birthdate
+const youngestBirthdate = new Date (today - minAge * oneYearMs);
+
+//convert both dates to milliseconds
+const oldestMs = oldestBirthdate.getTime();
+const youngestMs = youngestBirthdate.getTime();
+
+//pick a random value between the oldest and youngest birthdate
+const randomMs = oldestMs + Math.random() * (youngestMs - oldestMs);
 return dtoOut;
 }
 
